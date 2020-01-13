f6ce80417e6ba89e8059ca405cc50ed1bf4ae421
tkmct <1220t.takamichi@gmail.com>
2020-01-13 18:01:12 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,12 @@ module.exports = {
   // Customize your site metadata:
   siteMetadata: {
     title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    author: `Takamichi Tsutsumi`,
+    description: `Programmer`,
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
-      },
-      {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/tkmct`,
       },
     ],
   },
